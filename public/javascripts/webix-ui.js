e1b84ca0e41e5145f218dbb2a0d63c9f4f968245
Toby Ealden <toby.ealden@gmail.com>
2015-06-12 15:51:18 +0100

    Close all accordion panels rathern than hiding it on feature view.

--- public/javascripts/webix-ui.js
+++ public/javascripts/webix-ui.js
@@ -415,7 +415,8 @@ centrePoint.uiMainLayout = {
               headerHeight: centrePoint.barHeight,
               collapsed: true,
               body: centrePoint.uiRiskFactorsSideBar
-            }
+            },
+            { gravity: 0.01 }
           ]
         }
       ]
